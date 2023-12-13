defc06c62db6578d5b19926d9ab70348115534fe
Jirasak Tumbal <92836354+breezjirasak@users.noreply.github.com>
2023-12-14 00:10:34 +0700

    Update config.py

--- exercise-and-nutrition-backend/config.py
+++ exercise-and-nutrition-backend/config.py
@@ -1,6 +1,6 @@
 OPENAPI_STUB_DIR = 'stub'
-DB_HOST = "iot.cpe.ku.ac.th"
+DB_HOST = DATABASE HOST
 
-DB_USER = "b6410546114"
-DB_PASSWD = "jirasak.tu@ku.th"
-DB_NAME = "group12"
+DB_USER = USER
+DB_PASSWD = PASSWORD
+DB_NAME = DATABASE NAME
